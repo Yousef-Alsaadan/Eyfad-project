0175d96d00ab44860677158dea781898c519047c
unknown <madaawadh1@gmail.com>
2024-10-20 12:05:58 +0300

    user schema +user controller

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors'); 
 const uploadRoute = require('./routes/uploadRoute'); // Import the upload route
-
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 5000;
 
 // Enable CORS for all routes
 app.use(cors());
-
+app.use(express.json())
 
 app.use('/upload', uploadRoute);
 
