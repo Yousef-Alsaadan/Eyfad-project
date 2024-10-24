e519870d000eeff024f7752004ed3894b041953b
noura <noura.tu19@gmail.com>
2024-10-24 21:26:30 +0300

    restyle wtitng and add css animation

--- frontend/src/pages/Witing.jsx
+++ frontend/src/pages/Witing.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Title from "../Components/Title";
 import witingImage from "../images/witing.png";
+import WitingSVG from "../Components/style/WitingSvg.jsx"
 
 const Witing = () => {
   return (
     <div className="container mx-auto w-full text-center p-4 h-screen">
       <Title title="نقوم الآن بتحليل نتيجتك ..." />
       <div className="flex justify-center items-center mt-4 h-[85%]">
-        <img
+        {/* <img
           src={witingImage}
           alt="تحليل النتائج باستخدام الذكاء الاصطناعي"
           className={`w-full md:w-1/2 lg:w-1/3 xl:w-1/4 object-cover transition-transform duration-300 hover:scale-105 move-up`}
-        />
+        /> */}
+        <div    className={`  transition-transform duration-300 hover:scale-105 move-up`}>
+        <WitingSVG />
+        </div>
+
       </div>
     </div>
   );
