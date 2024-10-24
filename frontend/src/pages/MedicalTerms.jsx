bc96234d2528674f5c30aad0a794359d7fd7eeff
noura <noura.tu19@gmail.com>
2024-10-24 22:13:19 +0300

    restyle MedicalTerms

--- frontend/src/pages/MedicalTerms.jsx
+++ frontend/src/pages/MedicalTerms.jsx
@@ -68,11 +68,13 @@ function MedicalTerms() {
   };
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-custom-gradient">
       <NavBar />
       <div className="flex-grow container mx-auto w-full text-center my-10 px-4">
-        <Title title="ابحث عن مصطلح طبي تود المعرفة عنه" linkPath="/" />
-        <div className="flex flex-col items-center p-6 space-y-6">
+      <Title 
+  title={<span>ابحث عن <span   className="highlight">المصطلح الطبي</span> تود المعرفة عنه</span>} 
+  linkPath="/" 
+/>        <div className="flex flex-col items-center p-6 space-y-6">
           
           {/* Search Bar */}
           <div className="relative w-full max-w-lg mb-10">
