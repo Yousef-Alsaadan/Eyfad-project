84164c65855a32a4dfbf1af9c0ffef58be7e29b3
unknown <madaawadh1@gmail.com>
2024-10-27 13:40:37 +0300

    fixed port

--- frontend/src/Components/FileUpload.jsx
+++ frontend/src/Components/FileUpload.jsx
@@ -69,7 +69,7 @@ const FileUpload = ({ token, onUploadComplete }) => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5002/upload",
+        "http://localhost:5000/upload",
         formData,
         {
           headers: {
